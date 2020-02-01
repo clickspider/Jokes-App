8b3a493824d721f5c0a93210e09e18d4bd2b7f94
clickspider <danielfrey101@gmail.com>
2020-02-01 12:02:13 +0100

    Server.js(Login updated)

--- server/server.js
+++ server/server.js
@@ -26,24 +26,6 @@ app.get("/joke", async (req, res) => {
   }
 });
 
-app.get("/login/:userAndPass", async (req, res) => {
-  try {
-    const userAndPass = req.params.userAndPass.split(",");
-    const username = userAndPass[0];
-    const password = userAndPass[1];
-    const api_url = "http://localhost:4000/login";
-    const fetch_res = await axios.post(api_url, {
-      username,
-      password
-    });
-    res.send(fetch_res.data);
-  } catch (err) {
-    return res.status(err.response.status).json({
-      err: err.response.data.err
-    });
-  }
-});
-
 app.get("/profile/:token", async (req, res) => {
   try {
     const TOKEN = req.params.token;
